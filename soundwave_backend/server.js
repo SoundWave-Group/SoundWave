41340b3f9bf2765cf6254d3528dda9e2d06bb787
sarf01k <isaacksarfo1@gmail.com>
2024-06-08 18:18:38 +0000

    first commit

---
+++ soundwave_backend/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+require ("dotenv").config();
+const morgan = require('morgan');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(morgan('dev'));
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.R_UR
+});
+
+app.get('/login', (req, res) => {
+    const scopes = ['user-read-private', 'user-read-email', 'user-read-playback-state', 'user-modify-playback-state'];
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+})
+
+app.get('/callback', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (error) {
+        console.error(`Error:`, error);
+        res.send(`Error: ${error}`);
+        return;
+    }
+
+    spotifyApi.authorizationCodeGrant(code).then(data=>{
+        const accessToken = data.body['access_token'];
+        const refreshToken = data.body['refresh_token'];
+        const expiresIn = data.body['expires_in'];
+
+        spotifyApi.setAccessToken(accessToken);
+        spotifyApi.setRefreshToken(refreshToken);
+
+        console.log(accessToken, refreshToken);
+        res.send('Success');
+
+        setInterval(async () => {
+            const data = await spotifyApi.refreshAccessToken();
+            const accessTokenRefreshed = data.body['access_token'];
+            spotifyApi.setAccessToken(accessTokenRefreshed);
+        }, expiresIn/2*1000);
+    }).catch(error => {
+        console.error(`Error:`, error);
+        res.send('Error getting token');
+    });
+});
+
+app.get('/search', (req, res) => {
+    const {q} = req.query;
+    spotifyApi.searchTracks(q).then(searchData => {
+        const trackUrl = searchData.body.tracks.items[0].uri;
+        res.send({uri: trackUrl});
+    }).catch(err => {
+        console.error('Search Error:', err);
+        res.send('Error occurred during search');
+    });
+});
+
+app.get('/play', (req, res) => {
+
+    const { uri } = req.query;
+
+    spotifyApi.play({ uris: [uri] }).then(() => {
+        res.send('Playback started');
+    }).catch(err => {
+        console.error('Play Error:', err);
+        res.send('Error occurred during playback');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+})
