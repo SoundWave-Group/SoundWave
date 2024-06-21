ae827db5a05c0f33a187690662339983f64e7375
sarf01k <isaacksarfo1@gmail.com>
2024-06-21 13:49:51 +0000

    added login + signup with email

--- soundwave_backend/controllers/user.js
+++ soundwave_backend/controllers/user.js
@@ -1,84 +1,53 @@
 const User = require('../models/user.model');
-const bcrypt = require('bcrypt');
 
 exports.getUsers = async (req, res) => {
     try {
         const users = await User.find();
         return res.status(200).json({
             users: users
-        })
+        });
     } catch (error) {
         console.error(`Error:\n${error}`);
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
-exports.signUp = async (req, res) => {
+exports.deleteUser = async (req, res) => {
     try {
-        const { fullName, username, email, password, confirmPassword } = req.body;
+        const { userId } = req.params;
 
-        if (!username || !email || !password || !confirmPassword) {
-    		return res.status(400).json({ message: 'please fill all fields' })
-    	}
+        const user = await User.findOne({ _id: userId });
 
-        const existingUser = await User.findOne({ username })
-
-    	if (existingUser) {
-    		return res.status(400).json({ message: 'user already exists' })
-    	}
-
-        if (confirmPassword !== password) {
-            return res.status(400).json({ message: 'invalid confirm password' })
+        if (!user) {
+            return res.status(400).json({ message: "user does not exist" });
         }
 
-        const salt = await bcrypt.genSalt(10);
-    	const hashedPassword = await bcrypt.hash(password, salt);
+        await User.deleteOne({ _id: userId });
 
-        const user = await User.create({
-            fullName,
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        res.status(200)
-            .json({
-                message: 'signed up successfully',
-                user: user
-            });
+        return res.status(200).json({
+            message: "user deleted"
+        })
     } catch (error) {
-        console.log(`Error:\n${error}`)
-		return res.status(500).json({ message: 'internal server error' })
+        console.log(`Error:\n${error}`);
+		return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
-exports.login = async (req, res) => {
+exports.deleteAllUsers = async (req, res) => {
     try {
-        const { username, password } = req.body
+        const users = await User.find();
 
-        if ( !username || !password ) {
-    		return res.status(400).json({ message: 'please fill all fields' })
-    	}
-
-        const user = await User.findOne({ username });
-
-        if (!user) {
-            return res.status(400).json({ message: 'user not found' })
+        if (!users) {
+            return res.status(400).json({ message: "there are no users" })
         }
 
-        const match = await bcrypt.compare(password, user.password)
+        await User.deleteMany()
 
-        if (!match) {
-            return res.status(401).json({ message: 'incorrect password' })
-        }
-console.log(user);
-        res.status(200)
-            .json({
-                message: 'logged in successfully',
-                user: user
-            });
+        return res.status(200).json({
+            message: "all users deleted"
+        })
     } catch (error) {
         console.log(`Error:\n${error}`)
-		res.status(500).json({ message: 'internal server error' })
+		return res.status(500).json({ success: false, message: "Internal server error" })
     }
 }
