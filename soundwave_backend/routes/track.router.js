d5a2d17b67b88d3c58e43cde1e6efef3f5a42799
sarf01k <isaacksarfo1@gmail.com>
2024-07-30 22:33:13 +0000

    feat: upload track

--- soundwave_backend/routes/track.router.js
+++ soundwave_backend/routes/track.router.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const {
-  postTrack,
   getAllTracks,
   getTrack,
   updateTrack,
   deleteTrack,
   uploadTrack,
 } = require('../controllers/track.controller.js');
-const multer = require('multer');
-const upload = multer({ dest: './uploads/tracks/' });
+const upload = require('../middleware/multer.js');
 
 router.post('/upload', upload.single('track'), uploadTrack); //upload a track
 router.get('/', getAllTracks); //get all tracks
